750e32e90703b84f7d92884940f61b9100e8257c
AtmoFX <154053326+AtmoFX@users.noreply.github.com>
2024-01-08 23:23:07 +0800

    Rename bigint.h to source/bigint.h

--- source/bigint.h
+++ source/bigint.h
@@ -5,7 +5,7 @@
 /*    bigint Library for C++                                                            */
 /*                                                                                      */
 /*    MIT License                                                                       */
-/*    Copyright (c) 2023-2024 François-Xavier David                                     */
+/*    Copyright (c) 2023-2024 FranÃ§ois-Xavier David                                     */
 /*                                                                                      */
 /*    Permission is hereby granted, free of charge, to any person obtaining a copy      */
 /*    of this software and associated documentation files (the "Software"), to deal     */
